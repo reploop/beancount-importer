2d773127ef8c07bebbb7c7036d1c35b494f27069
George Cao <matrix3456@gmail.com>
2023-09-05 09:13:13 +0800

    Init project

---
+++ src/main/java/org/reploop/beancount/AlipayImporter.java
@@ -0,0 +1,12 @@
+package org.reploop.beancount;
+
+public class AlipayImporter {
+
+    public static void main(String... args) {
+        AlipayImporter importer = new AlipayImporter();
+        importer.importCsv();
+    }
+
+    public void importCsv() {
+    }
+}
