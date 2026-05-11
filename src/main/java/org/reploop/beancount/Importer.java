89e05f3adcef3ae7556d97ae8be72cd377837c35
George Cao <matrix3456@gmail.com>
2026-05-11 23:27:39 +0800

    output beancounts

--- src/main/java/org/reploop/beancount/Importer.java
+++ src/main/java/org/reploop/beancount/Importer.java
@@ -27,10 +27,11 @@ import static java.nio.file.StandardOpenOption.WRITE;
 public class Importer {
     public static void main(String... args) {
         var billImporters = getBillImporters().stream().collect(Collectors.groupingBy(BillImporter::platform, Collectors.toList()));
-        Path dir = Paths.get("/Users/george/personal-projects/beancount/bills");
+        Path scanDir = Paths.get("/Users/gc/Downloads/bills");
+        Path outDir = Paths.get("/Users/gc/Downloads/bills");
 
         DateTimeFormatter yearMonth = DateTimeFormatter.ofPattern("yyyy_MM");
-        try (var s = Files.find(dir, Integer.MAX_VALUE, (p, attrs) -> Files.isReadable(p))) {
+        try (var s = Files.find(scanDir, Integer.MAX_VALUE, (p, attrs) -> Files.isReadable(p))) {
             var files = s.toList();
             billImporters.forEach((platform, list) -> {
                 List<Transaction> transactions = new ArrayList<>();
@@ -45,25 +46,26 @@ public class Importer {
                 });
                 var monthList = transactions.stream()
                         .sorted(Comparator.comparing(Transaction::getDateTime).reversed())
+                        .distinct()
                         .collect(Collectors.groupingBy(txn -> txn.getDateTime().format(yearMonth), Collectors.toList()));
-                output(platform, monthList);
+                output(outDir, platform, monthList);
             });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void output(Platform platform, Map<String, List<Transaction>> txnList) {
-        Path dir = Paths.get("/Users/george/personal-projects/beancount/bills");
+    private static void output(Path dir, Platform platform, Map<String, List<Transaction>> txnList) {
         txnList.forEach((ym, list) -> {
-            String filename = platform.name() + "_" + ym + ".beancount";
+            String filename = platform.name().toLowerCase() + "_" + ym + ".beancount";
             var path = dir.resolve(filename);
             try (var writer = Files.newBufferedWriter(path, UTF_8, CREATE, TRUNCATE_EXISTING, WRITE)) {
                 for (var txn : list) {
                     writer.write(txn.toString());
+                    writer.newLine();
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                log.error("Cannot write file {} , platform: {}", path, platform, e);
             }
         });
     }
@@ -74,6 +76,6 @@ public class Importer {
         WechatXlsxImporter xlsxImporter = new WechatXlsxImporter();
         MeiTuanCsvImporter mtCsvImporter = new MeiTuanCsvImporter();
         JdCsvImporter jdImporter = new JdCsvImporter();
-        return List.of(jdImporter);
+        return List.of(alipayImporter, mtCsvImporter);
     }
 }
